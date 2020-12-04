ecd38021d1a999aa34521de86c84e63917dae4c2
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-04 15:36:56 -0300

    update

--- mirum/src/App.js
+++ mirum/src/App.js
@@ -1,28 +1,36 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
-import HomeUser from "./screens/HomeUser/HomeUser";
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+} from "react-router-dom";
 import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
-import './index.css'
+import HomeUser from "./screens/HomeUser/HomeUser";
+import Register from './screens/Register/Register.js'
 
 class App extends React.Component {
   handlePage = (url) => {
-    this.props.history.push(url)
+    this.props.history.push(url);
   };
+
   render() {
-    console.log(this.props)
     return (
       <main>
-        <ParticlesBackground />
+        <ParticlesBackground/>
         <Switch>
-          <Route path="/">
-            <HomeUser handlePage={this.handlePage}/>
+          <Route path="/" exact>
+            <HomeUser handlePage={this.handlePage} />
           </Route>
-          <Route path="/register"></Route>
+          <Route path="/register">
+            <Register/>
+          </Route>
           <Route path="/confirm"></Route>
-        </Switch>
-      
-      </main>
 
+        </Switch>
+
+      </main>
     );
   }
 }
