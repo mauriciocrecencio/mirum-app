c60c097ee52dd9a918d95c16eae3740ba00ebd4f
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-07 01:31:52 -0300

    improvements register screen

--- mirum/src/screens/Register/Register.js
+++ mirum/src/screens/Register/Register.js
@@ -2,24 +2,39 @@ import React from "react";
 import FormUser from "../../components/FormUser/FormUser";
 import "./Register.scss";
 import ParticlesBackground from "../../components/ParticlesBackground/ParticlesBackground";
-import InputFile from '../../components/ElementsHTML/InputFile/InputFile'
-
+import InputFile from "../../components/ElementsHTML/InputFile/InputFile";
+import { connect } from "react-redux";
+import { submitUserData, submitPhoto } from "../../actions/index";
 class Register extends React.Component {
   render() {
     return (
-      <div>
-        <ParticlesBackground />
-        <div className="container__register">
-          <aside className="container__picture">
-          <InputFile/>
-          </aside>
-          <section className="container__form">
-            <FormUser />
-          </section>
-        </div>
+      <div className="container__register">
+        <InputFile
+          photo={this.props.photo}
+          submitPhoto={this.props.submitPhoto}
+          getUserImg={this.props.getUserImg}
+        />
+    
+    <div className="vertical__row"/>
+      
+    
+        <FormUser
+          user={this.props.user}
+          onSubmit={this.props.submitUserData}
+          handlePage={this.props.handlePage}
+        />
       </div>
     );
   }
 }
+const mapStateToProps = (store) => ({
+  user: store.user,
+  photo: store.user.photo,
+});
 
-export default Register;
+const mapDispatchToProps = (dispatch) => ({
+  submitUserData: (user) => dispatch(submitUserData(user)),
+  submitPhoto: (photo) => dispatch(submitPhoto(photo)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
