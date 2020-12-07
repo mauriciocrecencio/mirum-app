fa5839910c7eb843b2fec68a47ea7a0a1f1ee2c4
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-07 12:18:31 -0300

    finish screen add

---
+++ mirum/src/screens/Finish/Finish.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Finish.scss'
+
+class Finish extends React.Component {
+  render() {
+    return (
+      <div className="finish">
+          <span>
+            Obrigado pelo seu cadastro.
+          </span>
+          <br/>
+        <span>Em breve seu prêmio chegará em sua casa!</span>
+      </div>
+    );
+  }
+}
+
+
+export default Finish;
