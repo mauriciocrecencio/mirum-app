d2031cbfa363b352822e863e99e6b976b0e565bc
Mauricio Crecencio <mauriciocrecencio98@gmail.com>
2020-12-07 01:33:00 -0300

    improvements

--- mirum/src/App.js
+++ mirum/src/App.js
@@ -8,7 +8,8 @@ import {
 } from "react-router-dom";
 import ParticlesBackground from "./components/ParticlesBackground/ParticlesBackground";
 import HomeUser from "./screens/HomeUser/HomeUser";
-import Register from './screens/Register/Register.js'
+import Register from "./screens/Register/Register.js";
+import Confirm from "./screens/Confirm/Confirm";
 
 class App extends React.Component {
   handlePage = (url) => {
@@ -18,17 +19,18 @@ class App extends React.Component {
   render() {
     return (
       <main>
+
         <Switch>
           <Route path="/" exact>
             <HomeUser handlePage={this.handlePage} />
           </Route>
           <Route path="/register">
-            <Register handlePage={this.handlePage}/>
+            <Register handlePage={this.handlePage} />
           </Route>
-          <Route path="/confirm"></Route>
-
+          <Route path="/confirm">
+            <Confirm history={this.props.history} />
+          </Route>
         </Switch>
-
       </main>
     );
   }
